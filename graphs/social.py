d2fee334e894eb0fd7a49223f308d7db68191506
Tom Tarpey <tomtarpeydev@gmail.com>
2021-03-11 19:56:53 +0000

    random sampling

--- graphs/social.py
+++ graphs/social.py
@@ -1,4 +1,5 @@
-from random import shuffle
+import random
+import time
 
 class Queue:
     def __init__(self):
@@ -29,12 +30,13 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            return False # print("WARNING: You cannot be friends with yourself")
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            return False # print("WARNING: Friendship already exists")
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -44,7 +46,7 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
-    def populate_graph(self, num_users, avg_friendships):
+    def populate_graph(self, num_users, avg_friendships): # O(n^2)
         """
         Takes a number of users and an average number of friendships
         as arguments
@@ -68,12 +70,21 @@ class SocialGraph:
         # create a list of possible friendships
         possible_friendships = []
 
+        # 1, [2, 3, 4, 5, 6, 7, 8, 9]
+        # 2, [3, 4, 5, 6, 7, 8, 9]
+        # 3, [4, 5, 6, 7, 8, 9]
+        # 4, [5, 6, 7, 8, 9]
+        # 5, [6, 7, 8, 9]
+        # 6, [7, 8, 9]
+        # 7, [8, 9]
+        # 8, [9]
+        # 9, []
         # to avoid duplicates make sure that the first number is smaller than the second
         for user_id in self.users:
             for friend_id in range(user_id + 1, self.last_id + 1):
                 possible_friendships.append((user_id, friend_id))
         # shuffle all possible friendship
-        shuffle(possible_friendships)
+        random.shuffle(possible_friendships)
         # create friendships for the first x pairs in the list of friends
         # X determined by the formula: num_users * avg_friendships // 2
         for i in range(num_users * avg_friendships // 2):
@@ -82,6 +93,39 @@ class SocialGraph:
             friend_id = friendship[1]
             self.add_friendship(user_id, friend_id)
 
+    def populate_graph_l(self, num_users, avg_friendships): # O(n)
+        """
+        Takes a number of users and an average number of friendships
+        as arguments
+
+        Creates that number of users and a randomly distributed friendships
+        between those users.
+
+        The number of users must be greater than the average number of friendships.
+        """
+        # Reset graph
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+        # !!!! IMPLEMENT ME
+        
+        # Add users
+        for i in range(0, num_users):
+            self.add_user(f"User {i+1}")
+
+        target_friendships = (num_users * avg_friendships)
+        total_friendships = 0
+        collisions = 0
+
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
+        
+        print(f"COLLISIONS: {collisions}")
 
     def get_all_social_paths(self, user_id):
         """
@@ -123,8 +167,24 @@ class SocialGraph:
 
 
 if __name__ == '__main__':
+    num_users = 2000
+    avg_friendships = 1999
+
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    start_time = time.time()
+    sg.populate_graph_l(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Linear Runtime: {end_time - start_time} seconds")
+
+    sg = SocialGraph()
+    start_time = time.time()
+    sg.populate_graph(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Quadratic Runtime: {end_time - start_time} seconds")
+
+
+
+    # sg.populate_graph(10, 2)
+    # print(sg.friendships)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
